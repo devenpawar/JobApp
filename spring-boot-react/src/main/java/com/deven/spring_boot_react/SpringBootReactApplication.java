3fb28acda86200db46fe6f367d094c639b2b5a99
Deven Pawar <30934682+devenpawar@users.noreply.github.com>
2024-11-07 21:20:42 +0530

    Backend updated

---
+++ spring-boot-react/src/main/java/com/deven/spring_boot_react/SpringBootReactApplication.java
@@ -0,0 +1,12 @@
+package com.deven.spring_boot_react;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReactApplication.class, args);
+		System.out.println("Hello my name is Deven!");
+	}
+}
